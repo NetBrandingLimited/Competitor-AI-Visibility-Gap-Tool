b3b895c5007294abb81a48e7b394f9a058cbf592
NetBrandingLimited <netbrandingmcc@gmail.com>
2026-04-28 12:59:40 +0530

    Changes

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from 'next';
 import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+  title: 'Auth'
+};
 
 export default function AuthRedirectPage() {
   redirect('/login');
